1d20a88bd4841927e5d43315df914ce8d2ac02ff
S.P. Mohanty <spmohanty91@gmail.com>
2014-12-05 18:41:40 +0530

    Reorganizing code

--- t4tc-stats-monitor.py
+++ t4tc-stats-monitor.py
@@ -3,6 +3,11 @@ import glob
 import redis
 import time
 from daemonize import Daemonize
+import string
+import random
+
+def getEntropy():
+	return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(16))
 
 
 redis_server = "t4tc-mcplots-db.cern.ch"
@@ -10,9 +15,9 @@ state_folder = "/nfs/shared/mcplots/state/"
 
 ######Data Types
 ##Volunteer
-volunteers_flush_TTL = 10  * 1000 #flush_TTL is the timegap in miliseconds. we can flush all the data before current_time - flush_TTL
+volunteers_flush_TTL = 10  #flush_TTL is the timegap in seconds. we can flush all the data before current_time - flush_TTL
 
-pending_flush_TTL = 10  * 1000 #flush_TTL is the timegap in miliseconds. we can flush all the data before current_time - flush_TTL
+pending_flush_TTL = 10  #flush_TTL is the timegap in seconds. we can flush all the data before current_time - flush_TTL
 
 
 
@@ -25,46 +30,44 @@ def main():
 			try:
 				queue_length += int(j)
 			except:
-				pass ##Silently pass in case of any errors in the jm file	
+				pass ##Silently pass in case of any errors in the jm file
 			f.close()
 
 		timeseries_data_push("T4TC_MONITOR/TOTAL/pending", "pending", queue_length)
-		
-		# #Get Volunteers
-		# f = open(state_folder+"volunteers")
-		# lines = f.readlines()
-		# if len(lines)>0:
-		# 	last_line = lines[-1]
-		# 	try:
-		# 		timeseries_data_push("T4TC_MONITOR/TOTAL/online_users", "volunteers", int(last_line))
-		# 	except:
-		# 		pass # Silently pass in case of errors in the file
 
-		time.sleep(1000); ## Updated every second
-	
+		#Get Volunteers
+		f = open(state_folder+"volunteers")
+		lines = f.readlines()
+		if len(lines)>0:
+			last_line = lines[-1]
+			try:
+				timeseries_data_push("T4TC_MONITOR/TOTAL/online_users", "volunteers", int(last_line))
+			except:
+				pass # Silently pass in case of errors in the file
+
+		time.sleep(1); ## Updated every second
+
 
 def timeseries_data_push(key, typ, data): #type == data type as mentioned in the block above
-	print key,typ, data
+	#print key, typ,data
 	r = redis.Redis(host=redis_server, port=6379, db=0)
 	p=r.pipeline()
 
 	## Push the data into the sorted set at key with timeestamp as score
-	p.zadd(key,int(time.time()), data)
-	
-	## Flush all data before flush_TTL miliseconds
+	#print key, int(time.time()), data
+	p.zadd(key, getEntropy()+"_"+str(data), int(time.time()))
+
+	## Flush all data before flush_TTL seconds
 	flush_TTL = 10**100
 	if(typ=="volunteers"):
 		flush_TTL = volunteers_flush_TTL
 	elif(typ=="pending"):
 		flush_TTL = pending_flush_TTL
-
-
+	#print int(time.time())-flush_TTL
 	p.zremrangebyscore(key, 0, int(time.time())-flush_TTL)
-	p.execute() 	
+	p.execute()
 
 # daemon = Daemonize(app="t4tc-stats-monitor", pid="/tmp/t4ts-stats-monitor", action=main)
 # daemon.start()
 main()
 #timeseries()
-
-
