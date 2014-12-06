ef406b2ac0805b6338c4e7f055254950bae1cdbe
S.P. Mohanty <spmohanty91@gmail.com>
2014-12-06 08:59:21 +0100

    Adding monitor* data points

--- t4tc-stats-monitor.py
+++ t4tc-stats-monitor.py
@@ -19,7 +19,7 @@ volunteers_flush_TTL = 10  #flush_TTL is the timegap in seconds. we can flush al
 
 pending_flush_TTL = 10  #flush_TTL is the timegap in seconds. we can flush all the data before current_time - flush_TTL
 
-
+monitor_machines_TTL = 10
 
 def main():
 	while 1:
@@ -45,6 +45,38 @@ def main():
 			except:
 				pass # Silently pass in case of errors in the file
 
+		#Get monitor-machines
+		f = open(state_folder+"monitor-machines")
+		lines = f.readlines()
+		if len(lines)>0:
+			last_line = lines[-1]
+			try:
+				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-machines/HIST", "monitor-machines", int(last_line))
+			except:
+				pass # Silently pass in case of errors in the file		
+
+		#Get monitor-load
+		f = open(state_folder+"monitor-load")
+		lines = f.readlines()
+		if len(lines)>0:
+			last_line = lines[-1]
+			try:
+				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-load/HIST", "monitor-load", int(last_line))
+			except:
+				pass # Silently pass in case of errors in the file									
+
+
+
+		#Get monitor-load
+		f = open(state_folder+"monitor-alerts")
+		lines = f.readlines()
+		if len(lines)>0:
+			last_line = lines[-1]
+			try:
+				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-alerts/HIST", "monitor-alerts", int(last_line))
+			except:
+				pass # Silently pass in case of errors in the file		
+
 		time.sleep(1); ## Updated every second
 
 
@@ -63,11 +95,18 @@ def timeseries_data_push(key, typ, data): #type == data type as mentioned in the
 		flush_TTL = volunteers_flush_TTL
 	elif(typ=="pending"):
 		flush_TTL = pending_flush_TTL
+	elif(typ=="monitor-machines"):
+		flush_TTL = monitor_machines_TTL	## User common TTL for monitor-* type data	
+	elif(typ=="monitor-load"):
+		flush_TTL = monitor_machines_TTL		
+	elif(typ=="monitor-alerts"):
+		flush_TTL = monitor_machines_TTL		
+
 	#print int(time.time())-flush_TTL
 	p.zremrangebyscore(key, 0, int(time.time())-flush_TTL)
 	p.execute()
 
-# daemon = Daemonize(app="t4tc-stats-monitor", pid="/tmp/t4ts-stats-monitor", action=main)
-# daemon.start()
-main()
+daemon = Daemonize(app="t4tc-stats-monitor", pid="/tmp/t4ts-stats-monitor", action=main)
+daemon.start()
+# main()
 #timeseries()
