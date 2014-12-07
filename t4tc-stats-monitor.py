e1f4f7b3dcfa5a6e88f8fe3fcdab45ca0b845f6e
S.P. Mohanty <spmohanty91@gmail.com>
2014-12-07 12:49:44 +0100

    Reorganizing code

--- t4tc-stats-monitor.py
+++ t4tc-stats-monitor.py
@@ -17,7 +17,9 @@ def getEntropy():
 redis_server = "t4tc-mcplots-db.cern.ch"
 state_folder = "/nfs/shared/mcplots/state/"
 
-every_t_seconds = 1*60*10 ## 10minutes
+debug_mode = False
+
+every_t_seconds = 60*60 ## 10minutes
 
 ######Data Types
 ##Volunteer
@@ -33,148 +35,186 @@ real_time_update_delay = 58
 
 def pending_update():
 	while 1:
-		error = False
-		queue_length = 0
-		for jm in glob.glob(state_folder+"jm_t4tc-copilot-jm-*"):
-			f = open(jm)
-			j = f.read()
-			try:
-				queue_length += int(j)
+		if debug_mode: print "In pending_update\n"
+		##pending
+		r = redis.Redis(host=redis_server, port=6379, db=0)
+		p=r.pipeline()
 
-			except:
-				error = True
-				print "Error in queue length ", queue_length," in file :: ", jm
-				pass ##Silently pass in case of any errors in the jm file
-			f.close()
+		p.hget("T4TC_MONITOR/TOTAL/", "pending_instant");
+		p.hget("T4TC_MONITOR/TOTAL/", "volunteers_instant");
+		p.hget("T4TC_MONITOR/TOTAL/", "monitor-machines_instant");
+		p.hget("T4TC_MONITOR/TOTAL/", "monitor-load_instant");
+		p.hget("T4TC_MONITOR/TOTAL/", "monitor-alerts_instant");
+		p.hget("T4TC_MONITOR/TOTAL/", "jobs_completed")
+		p.hget("T4TC_MONITOR/TOTAL/", "jobs_failed")
+
+		results = p.execute()
+		if debug_mode: print results
+
+		if len(results) == 7:
+			for k in range(len(results)):
+				if not results[k]: ##Change all None values to 0
+					results[k] = 0
+
+			try:		
+				timeseries_data_push("T4TC_MONITOR/TOTAL/pending/HIST", "pending", int(str(results[0]).strip()))
+				timeseries_data_push("T4TC_MONITOR/TOTAL/online_users/HIST", "volunteers", int(str(results[1]).strip()))
+				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-machines/HIST", "monitor-machines", int(str(results[2]).strip()))
+				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-load/HIST", "monitor-load", float(str(results[3]).strip()))
+				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-alerts/HIST", "monitor-alerts", int(str(results[4]).strip()))
+				timeseries_data_push("T4TC_MONITOR/TOTAL/jobs_completed/HIST", "jobs_completed" ,int(str(results[5]).strip())-int(str(results[6]).strip())) ##jobs_completed holds the total jobs succeded + failed
+				timeseries_data_push("T4TC_MONITOR/TOTAL/jobs_failed/HIST", "jobs_failed" ,int(str(results[6]).strip()))
+			except Exception as inst:
+				if debug_mode: print type(inst)     # the exception instance
+				if debug_mode: print inst.args
+
+		time.sleep(every_t_seconds)##Repeat this every two seconds
 
 
-		if not error:
-			## Update Queue Length in database
+def realtime_update():
+	if debug_mode: print "In realtime_update"
+
+	while 1:
 			r = redis.Redis(host=redis_server, port=6379, db=0)
 			p=r.pipeline()
-			p.hset("T4TC_MONITOR/TOTAL/", "pending_instant", queue_length)
-			p.execute()
+			if debug_mode: print "="*80
+			if debug_mode: print "In realtime_update"
 
-		time.sleep(real_time_update_delay)##Repeat this every two seconds
+			error = False
+			queue_length = 0
+			for jm in glob.glob(state_folder+"jm_t4tc-copilot-jm-*"):
+				f = open(jm)
+				j = f.read()
+				try:
+					queue_length += int(j)
+
+				except:
+					error = True
+					if debug_mode: print "Error in queue length ", queue_length," in file :: ", jm
+					pass ##Silently pass in case of any errors in the jm file
+				f.close()
+
+
+			if not error:
+				if debug_mode: print "queue_length", queue_length			
+				# timeseries_data_push("T4TC_MONITOR/TOTAL/pending/HIST", "pending", queue_length)
+				p.hset("T4TC_MONITOR/TOTAL/", "pending_instant", queue_length);
+
+
+			#Get Volunteers
+			f = open(state_folder+"volunteers")
+			lines = f.readlines()
+			if len(lines)>0:
+				last_line = lines[-1]
+				try:
+					# timeseries_data_push("T4TC_MONITOR/TOTAL/online_users/HIST", "volunteers", int(last_line))
+					if debug_mode: print "Volunteers :: ",int(last_line)
+					if not last_line:
+						last_line = 0
+
+					p.hset("T4TC_MONITOR/TOTAL/", "volunteers_instant", int(last_line));
+				except Exception as inst:
+					if debug_mode: print type(inst)     # the exception instance
+					if debug_mode: print inst.args      # arguments stored in .args
+					if debug_mode: print inst           # __str__ allows args to be if debug_mode: printed directly
+					x, y = inst.args
+					if debug_mode: print 'x =', x
+					if debug_mode: print 'y =', y
+					foo=1
+
+			#Get monitor-machines
+			f = open(state_folder+"monitor-machines")
+			lines = f.readlines()
+			if len(lines)>0:
+				last_line = lines[-1]
+				try:
+					# timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-machines/HIST", "monitor-machines", int(last_line))
+					if debug_mode: print "Monitor-Machines ::", int(last_line)
+					if not last_line:
+						last_line = 0
+					p.hset("T4TC_MONITOR/TOTAL/", "monitor-machines_instant", int(last_line));
+				except Exception as inst:
+					if debug_mode: print type(inst)     # the exception instance
+					if debug_mode: print inst.args      # arguments stored in .args
+					if debug_mode: print inst           # __str__ allows args to be if debug_mode: printed directly
+					x, y = inst.args
+					if debug_mode: print 'x =', x
+					if debug_mode: print 'y =', y
+					foo=1	
+
+			#Get monitor-load
+			f = open(state_folder+"monitor-load")
+			lines = f.readlines()
+			if len(lines)>0:
+				last_line = lines[-1]
+				try:
+					# timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-load/HIST", "monitor-load", float(last_line))
+					if debug_mode: print "Monitor-load ::", float(last_line)
+					if not last_line:
+						last_line = 0
+
+					p.hset("T4TC_MONITOR/TOTAL/", "monitor-load_instant", float(last_line));
+
+				except Exception as inst:
+					if debug_mode: print type(inst)     # the exception instance
+					if debug_mode: print inst.args      # arguments stored in .args
+					if debug_mode: print inst           # __str__ allows args to be if debug_mode: printed directly
+					x, y = inst.args
+					if debug_mode: print 'x =', x
+					if debug_mode: print 'y =', y
+					foo=1								
+
+
+
+			#Get monitor-load
+			f = open(state_folder+"monitor-alerts")
+			lines = f.readlines()
+			if len(lines)>0:
+				last_line = lines[-1]
+				try:
+					# timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-alerts/HIST", "monitor-alerts", int(last_line))
+					if debug_mode: print "Monitor-alerts ::", int(last_line)
+					p.hset("T4TC_MONITOR/TOTAL/", "monitor-alerts_instant", int(last_line));
+
+				except Exception as inst:
+					if debug_mode: print type(inst)     # the exception instance
+					if debug_mode: print inst.args      # arguments stored in .args
+					if debug_mode: print inst           # __str__ allows args to be if debug_mode: printed directly
+					x, y = inst.args
+					if debug_mode: print 'x =', x
+					if debug_mode: print 'y =', y
+					foo=1	
+
+			# #Get jobs completed and failed
+			# r = redis.Redis(host=redis_server, port=6379, db=0)
+			# p=r.pipeline()
+
+			results = p.execute()
+			if debug_mode: print results
+
+			time.sleep(real_time_update_delay); ## Updated every t seconds
 
 
 def main():
 
-	t = threading.Thread(target=pending_update)
-	t.start()
+	pupdate = threading.Thread(target=pending_update)
+	rupdate = threading.Thread(target=realtime_update)
 
-	while 1:
-		print "="*80
-		error = False
-		queue_length = 0
-		for jm in glob.glob(state_folder+"jm_t4tc-copilot-jm-*"):
-			f = open(jm)
-			j = f.read()
-			try:
-				queue_length += int(j)
-
-			except:
-				error = True
-				print "Error in queue length ", queue_length," in file :: ", jm
-				pass ##Silently pass in case of any errors in the jm file
-			f.close()
+	pupdate.start()
+	rupdate.start()
 
 
-		if not error:
-			print "queue_length", queue_length			
-			timeseries_data_push("T4TC_MONITOR/TOTAL/pending/HIST", "pending", queue_length)
-
-		#Get Volunteers
-		f = open(state_folder+"volunteers")
-		lines = f.readlines()
-		if len(lines)>0:
-			last_line = lines[-1]
-			try:
-				timeseries_data_push("T4TC_MONITOR/TOTAL/online_users/HIST", "volunteers", int(last_line))
-			except Exception as inst:
-				print type(inst)     # the exception instance
-				print inst.args      # arguments stored in .args
-				print inst           # __str__ allows args to be printed directly
-				x, y = inst.args
-				print 'x =', x
-				print 'y =', y
-				foo=1
-
-		#Get monitor-machines
-		f = open(state_folder+"monitor-machines")
-		lines = f.readlines()
-		if len(lines)>0:
-			last_line = lines[-1]
-			try:
-				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-machines/HIST", "monitor-machines", int(last_line))
-			except Exception as inst:
-				print type(inst)     # the exception instance
-				print inst.args      # arguments stored in .args
-				print inst           # __str__ allows args to be printed directly
-				x, y = inst.args
-				print 'x =', x
-				print 'y =', y
-				foo=1	
-
-		#Get monitor-load
-		f = open(state_folder+"monitor-load")
-		lines = f.readlines()
-		if len(lines)>0:
-			last_line = lines[-1]
-			try:
-				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-load/HIST", "monitor-load", float(last_line))
-			except Exception as inst:
-				print type(inst)     # the exception instance
-				print inst.args      # arguments stored in .args
-				print inst           # __str__ allows args to be printed directly
-				x, y = inst.args
-				print 'x =', x
-				print 'y =', y
-				foo=1								
-
-
-
-		#Get monitor-load
-		f = open(state_folder+"monitor-alerts")
-		lines = f.readlines()
-		if len(lines)>0:
-			last_line = lines[-1]
-			try:
-				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-alerts/HIST", "monitor-alerts", int(last_line))
-			except Exception as inst:
-				print type(inst)     # the exception instance
-				print inst.args      # arguments stored in .args
-				print inst           # __str__ allows args to be printed directly
-				x, y = inst.args
-				print 'x =', x
-				print 'y =', y
-				foo=1	
-
-		#Get jobs completed and failed
-		r = redis.Redis(host=redis_server, port=6379, db=0)
-		p=r.pipeline()
-
-		p.hget("T4TC_MONITOR/TOTAL/", "jobs_completed")
-		p.hget("T4TC_MONITOR/TOTAL/", "jobs_failed")
-
-		result = p.execute()
-		if len(result)==2:
-			timeseries_data_push("T4TC_MONITOR/TOTAL/jobs_completed/HIST", "jobs_completed" ,int(result[0])-int(result[1])) ##jobs_completed holds the total jobs succeded + failed
-			timeseries_data_push("T4TC_MONITOR/TOTAL/jobs_failed/HIST", "jobs_failed" ,int(result[1]))
-
-
-		time.sleep(every_t_seconds); ## Updated every t seconds
-
+	
 
 
 
 def timeseries_data_push(key, typ, data): #type == data type as mentioned in the block above
-	print key, typ,data
+	if debug_mode: print key, typ,data
 	r = redis.Redis(host=redis_server, port=6379, db=0)
 	p=r.pipeline()
 
 	## Push the data into the sorted set at key with timeestamp as score
-	#print key, int(time.time()), data
+	#if debug_mode: print key, int(time.time()), data
 	p.zadd(key, getEntropy()+"_"+str(data), int(time.time()))
 
 	## Flush all data before flush_TTL seconds
@@ -192,12 +232,13 @@ def timeseries_data_push(key, typ, data): #type == data type as mentioned in the
 	else:
 		flush_TTL = other_TTL
 
-	#print int(time.time())-flush_TTL
+	#if debug_mode: print int(time.time())-flush_TTL
 	p.zremrangebyscore(key, 0, int(time.time())-flush_TTL)
 	p.execute()
 
-
-daemon = Daemonize(app="t4tc-stats-monitor", pid="/tmp/t4ts-stats-monitor", action=main)
-daemon.start()
-# main()
+if debug_mode:
+	main()
+else:
+	daemon = Daemonize(app="t4tc-stats-monitor", pid="/tmp/t4ts-stats-monitor", action=main)
+	daemon.start()
 
