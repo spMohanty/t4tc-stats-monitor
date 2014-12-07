e5a2c1d1acea4308d51548fb7e2b68b557c6b309
S.P. Mohanty <spmohanty91@gmail.com>
2014-12-07 07:38:48 +0100

    Adding separate thread of pending realtime update and stats T-second update

--- t4tc-stats-monitor.py
+++ t4tc-stats-monitor.py
@@ -5,6 +5,10 @@ import time
 from daemonize import Daemonize
 import string
 import random
+import logging
+
+import threading
+
 
 def getEntropy():
 	return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(16))
@@ -13,7 +17,7 @@ def getEntropy():
 redis_server = "t4tc-mcplots-db.cern.ch"
 state_folder = "/nfs/shared/mcplots/state/"
 
-every_t_seconds = 1 * 60 * 10 ## 10minutes
+every_t_seconds = 1*60*10 ## 10minutes
 
 ######Data Types
 ##Volunteer
@@ -26,8 +30,38 @@ monitor_machines_TTL = 12 * every_t_seconds ## 12 * every_t_seconds seconds
 other_TTL = 12 * every_t_seconds
 
 
+def pending_update():
+	while 1:
+		error = False
+		queue_length = 0
+		for jm in glob.glob(state_folder+"jm_t4tc-copilot-jm-*"):
+			f = open(jm)
+			j = f.read()
+			try:
+				queue_length += int(j)
+
+			except:
+				error = True
+				print "Error in queue length ", queue_length," in file :: ", jm
+				pass ##Silently pass in case of any errors in the jm file
+			f.close()
+
+
+		if not error:
+			## Update Queue Length in database
+			r = redis.Redis(host=redis_server, port=6379, db=0)
+			p=r.pipeline()
+			p.hset("T4TC_MONITOR/TOTAL/", "pending", queue_length)
+			p.execute()
+
+		time.sleep(2)##Repeat this every two seconds
+
 
 def main():
+
+	t = threading.Thread(target=pending_update)
+	t.start()
+
 	while 1:
 		print "="*80
 		error = False
@@ -56,8 +90,14 @@ def main():
 			last_line = lines[-1]
 			try:
 				timeseries_data_push("T4TC_MONITOR/TOTAL/online_users/HIST", "volunteers", int(last_line))
-			except:
-				pass # Silently pass in case of errors in the file
+			except Exception as inst:
+				print type(inst)     # the exception instance
+				print inst.args      # arguments stored in .args
+				print inst           # __str__ allows args to be printed directly
+				x, y = inst.args
+				print 'x =', x
+				print 'y =', y
+				foo=1
 
 		#Get monitor-machines
 		f = open(state_folder+"monitor-machines")
@@ -66,8 +106,14 @@ def main():
 			last_line = lines[-1]
 			try:
 				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-machines/HIST", "monitor-machines", int(last_line))
-			except:
-				pass # Silently pass in case of errors in the file		
+			except Exception as inst:
+				print type(inst)     # the exception instance
+				print inst.args      # arguments stored in .args
+				print inst           # __str__ allows args to be printed directly
+				x, y = inst.args
+				print 'x =', x
+				print 'y =', y
+				foo=1	
 
 		#Get monitor-load
 		f = open(state_folder+"monitor-load")
@@ -76,8 +122,14 @@ def main():
 			last_line = lines[-1]
 			try:
 				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-load/HIST", "monitor-load", float(last_line))
-			except:
-				pass # Silently pass in case of errors in the file									
+			except Exception as inst:
+				print type(inst)     # the exception instance
+				print inst.args      # arguments stored in .args
+				print inst           # __str__ allows args to be printed directly
+				x, y = inst.args
+				print 'x =', x
+				print 'y =', y
+				foo=1								
 
 
 
@@ -88,8 +140,14 @@ def main():
 			last_line = lines[-1]
 			try:
 				timeseries_data_push("T4TC_MONITOR/TOTAL/monitor-alerts/HIST", "monitor-alerts", int(last_line))
-			except:
-				pass # Silently pass in case of errors in the file		
+			except Exception as inst:
+				print type(inst)     # the exception instance
+				print inst.args      # arguments stored in .args
+				print inst           # __str__ allows args to be printed directly
+				x, y = inst.args
+				print 'x =', x
+				print 'y =', y
+				foo=1	
 
 		#Get jobs completed and failed
 		r = redis.Redis(host=redis_server, port=6379, db=0)
@@ -105,6 +163,8 @@ def main():
 
 
 		time.sleep(every_t_seconds); ## Updated every t seconds
+
+
 
 
 def timeseries_data_push(key, typ, data): #type == data type as mentioned in the block above
